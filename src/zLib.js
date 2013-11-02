754306b3173925bf97a4e9a483b5df33d9556377
Emmanuel <m4nu@hotmail.fr>
2013-11-02 16:15:30 +0800

    Adds DOMready

--- src/zLib.js
+++ src/zLib.js
@@ -9,13 +9,71 @@
 			//this.el = z.statics.selector( node );
 		} else if ( typeof node === "object" && node.nodeType !== "undefined" ) {
 			this.el = node;
+			console.log(node);
 		}  else {
 			throw new Error( "Argument is of wrong type" );
 		}
 	};
 
+	/*!
+	* domready (c) Dustin Diaz 2012 - License MIT
+	* (Edited to remove dynamic naming )
+	* @todo make this lintable
+	*/
+	var domready = function (ready) {
+		var fns = [],
+			fn,
+			f = false,
+			doc = document,
+			testEl = doc.documentElement,
+			hack = testEl.doScroll,
+			domContentLoaded = 'DOMContentLoaded',
+			addEventListener = 'addEventListener',
+			onreadystatechange = 'onreadystatechange',
+			readyState = 'readyState',
+			loadedRgx = hack ? /^loaded|^c/ : /^loaded|c/,
+			loaded = loadedRgx.test(doc[readyState]);
+
+		function flush(f) {
+			loaded = 1;
+			while (f = fns.shift()) f();
+		}
+
+		doc[addEventListener] && doc[addEventListener](domContentLoaded, fn = function () {
+			doc.removeEventListener(domContentLoaded, fn, f);
+			flush();
+		}, f);
+
+		hack && doc.attachEvent(onreadystatechange, fn = function () {
+			if (/^c/.test(doc[readyState])) {
+				doc.detachEvent(onreadystatechange, fn);
+				flush();
+			}
+		});
+
+		return (ready = hack ?
+			function (fn) {
+				self != top ?
+				loaded ? fn() : fns.push(fn) :
+				function () {
+					try {
+						testEl.doScroll('left');
+					} catch (e) {
+						return setTimeout(function() { ready(fn) }, 50);
+					}
+					fn();
+				}();
+			} :
+			function (fn) {
+			loaded ? fn() : fns.push(fn);
+		});
+	}();
+
 	// Statics
 	z.statics = {
+
+		// Local reference to Dustin Diaz Domready ( code at the bottom )
+		domReady: domready,
 
 		addEvent: function() {
 			if ( window.addEventListener !== 'undefined' ) {
@@ -45,10 +103,21 @@
 			} else {
 				throw new Error( 'This browser is not supported' );
 			}
-		}()
-
+		}(),
 	};
 
+
+	z.prototype = {
+		on : function( evt, fn ) {
+			z.statics.addEvent( this.el, evt, fn );
+			return this;
+		},
+
+		off : function( evt, fn ) {
+			z.statics.removeEvent( this.el, evt, fn );
+			return this;
+		}
+	};
 
 
 	window.z = z;
--- tests/zLib.test.js
+++ tests/zLib.test.js
@@ -1,6 +1,6 @@
 describe( 'zLib', function() {
 	it( 'should be available as a global object', function() {
-		expect( window.z ).toBeDefined();
+		expect( z ).toBeDefined();
 	});
 	describe( 'Initialization', function() {
 		it('Should force new', function() {
@@ -11,34 +11,57 @@ describe( 'zLib', function() {
 		it('Should throw if parameter is not passed', function() {
 			expect(function() { z() }).toThrow( new Error( "Argument is of wrong type" ) );
 		});
-
 	});
 
 
 	describe( 'statics', function() {
 		it( 'namespace should be defined', function() {
-			expect( window.z.statics ).toBeDefined();
+			expect( z.statics ).toBeDefined();
+		});
+
+		it( 'should have a domReady method', function() {
+			expect( z.statics.domReady ).toBeDefined();
 		});
 
 		describe( 'addEvent', function() {
 			it( 'should be defined', function() {
-				expect( window.z.statics.addEvent ).toBeDefined();
+				expect( z.statics.addEvent ).toBeDefined();
 			});
 			it( 'should use addEventListener if available', function() {
 				var stub = sinon.stub( document, 'addEventListener');
-				window.z.statics.addEvent( document );
+				z.statics.addEvent( document );
 				expect( stub.called ).toBe( true );
+				document.addEventListener.restore();
+			});
+			it( 'should successfully bind event to element', function() {
+				var spy = sinon.spy();
+				var event = new Event('click');
+				z.statics.addEvent( document, 'click', spy);
+				document.dispatchEvent( event );
+				expect( spy.called ).toBe( true );
 			});
 		});
 
 		describe( 'removeEvent', function() {
 			it( 'should be defined', function() {
-				expect( window.z.statics.removeEvent ).toBeDefined();
+				expect( z.statics.removeEvent ).toBeDefined();
 			});
 			it( 'should use removeEventListener if available', function() {
 				var stub = sinon.stub( document, 'removeEventListener');
-				window.z.statics.removeEvent( document );
+				z.statics.removeEvent( document );
 				expect( stub.called ).toBe( true );
+				document.removeEventListener.restore();
+			});
+		});
+	});
+
+	describe( 'API', function() {
+		describe( 'on method', function() {
+			it( 'should call static addEvent', function() {
+				var spy = sinon.spy( document, 'addEventListener');
+				z( document ).on( 'click' );
+				expect( spy.calledWith('click') ).toBe( true );
+				document.addEventListener.restore();
 			});
 		});
 	});
