9de9d3e209a2141f7b1500b29862aee09ae10e01
Emmanuel <m4nu@hotmail.fr>
2013-11-02 15:06:21 +0800

    Adds simple event API

--- src/zLib.js
+++ src/zLib.js
@@ -1,11 +1,51 @@
 (function( window, doc, undefined ) {
+
 	// Constructor
-	var z = {};
+	var z = function ( node ) {
+		if ( this === window ) { 
+			return new z( node );
+		}
+		if (  typeof node === "string" ) {
+			//this.el = z.statics.selector( node );
+		} else if ( typeof node === "object" && node.nodeType !== "undefined" ) {
+			this.el = node;
+		}  else {
+			throw new Error( "Argument is of wrong type" );
+		}
+	};
 
 	// Statics
 	z.statics = {
 
-		addEvent: function() {}
+		addEvent: function() {
+			if ( window.addEventListener !== 'undefined' ) {
+				return function( trg, evt, fn ) {
+					trg.addEventListener( evt, fn, false );
+				};
+			} else if ( typeof window.attachEvent !== 'undefined' ) {
+				return function( trg, evt, fn ) {
+					trg.attachEvent( evt, function() {
+						call( trg );
+					}, false );
+				};
+			} else {
+				throw new Error( 'This browser is not supported' );
+			}
+		}(),
+
+		removeEvent: function() {
+			if ( window.removeEventListener !== 'undefined' ) {
+				return function( trg, evt, fn ) {
+					trg.removeEventListener( evt, fn, false );
+				};
+			} else if ( typeof window.detachEvent !== 'undefined' ) {
+				return function( trg, evt, fn ) {
+					trg.detachEvent( evt, fn, true);
+				};
+			} else {
+				throw new Error( 'This browser is not supported' );
+			}
+		}()
 
 	};
 
--- tests/zLib.test.js
+++ tests/zLib.test.js
@@ -2,6 +2,17 @@ describe( 'zLib', function() {
 	it( 'should be available as a global object', function() {
 		expect( window.z ).toBeDefined();
 	});
+	describe( 'Initialization', function() {
+		it('Should force new', function() {
+			var instance =  z('hello');
+			var newInstance = new z('hello');
+			expect( instance ).toEqual( newInstance );
+		});
+		it('Should throw if parameter is not passed', function() {
+			expect(function() { z() }).toThrow( new Error( "Argument is of wrong type" ) );
+		});
+
+	});
 
 
 	describe( 'statics', function() {
@@ -12,6 +23,22 @@ describe( 'zLib', function() {
 		describe( 'addEvent', function() {
 			it( 'should be defined', function() {
 				expect( window.z.statics.addEvent ).toBeDefined();
+			});
+			it( 'should use addEventListener if available', function() {
+				var stub = sinon.stub( document, 'addEventListener');
+				window.z.statics.addEvent( document );
+				expect( stub.called ).toBe( true );
+			});
+		});
+
+		describe( 'removeEvent', function() {
+			it( 'should be defined', function() {
+				expect( window.z.statics.removeEvent ).toBeDefined();
+			});
+			it( 'should use removeEventListener if available', function() {
+				var stub = sinon.stub( document, 'removeEventListener');
+				window.z.statics.removeEvent( document );
+				expect( stub.called ).toBe( true );
 			});
 		});
 	});
